3c8de498aa621095a06668e455ba316fa4e24141
Xubili <guivpw68@gmail.com>
2021-02-21 17:23:24 -0300

    feat: add get many items repository

--- src/app/infra/database/prisma/itemRepository/item.ts
+++ src/app/infra/database/prisma/itemRepository/item.ts
@@ -1,10 +1,16 @@
-import { GetItemRepository } from '@data/protocols/getItemRepository'
+import { GetItemRepository } from '@data/protocols/itemRepository'
 import { ItemModel } from '@domain/models/item'
 import { PrismaHelper } from '../helpers/prismaHelper'
 
 export class ItemPrismaRepository implements GetItemRepository {
 	async get(id: string): Promise<ItemModel> {
 		const result = await PrismaHelper.findOneItem({ id })
+
+		return result
+	}
+
+	async getMany(): Promise<ItemModel[]> {
+		const result = await PrismaHelper.findManyItems()
 
 		return result
 	}
