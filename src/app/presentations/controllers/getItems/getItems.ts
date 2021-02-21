84fe8aa31108a5c1502a8db5fe671cddc7239286
Xubili <guivpw68@gmail.com>
2021-02-21 17:24:16 -0300

    feat:  add get many items controller

---
+++ src/app/presentations/controllers/getItems/getItems.ts
@@ -0,0 +1,17 @@
+import { HttpRequest, HttpResponse, Controller, GetItems } from './getItemsProtocols'
+import { ok, serverError } from '../../helpers/http-helper'
+
+export class GetItemsController implements Controller {
+	constructor(private getItems: GetItems) {}
+
+	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+		try {
+			const items = await this.getItems.get()
+
+			return ok({ data: items })
+		} catch (error) {
+			console.log(error)
+			return serverError()
+		}
+	}
+}
---
+++ src/app/presentations/controllers/getItems/getItemsProtocols.ts
@@ -0,0 +1,3 @@
+export * from '@presentations/protocols'
+export * from '@domain/useCases/items/getItems'
+export * from '@domain/models/item'
