51f96c79d142396f773c7559a5818c0d58951181
Xubili <guivpw68@gmail.com>
2021-02-21 17:24:34 -0300

    feat: add get many items resolver and type definition

--- src/app/main/graphql/resolvers/items.ts
+++ src/app/main/graphql/resolvers/items.ts
@@ -1,8 +1,10 @@
 import { adaptResolver } from '@main/adapters'
 import { makeGetItemController } from '@main/factories/items/getItem'
+import { makegetItemsController } from '@main/factories/items/getItems'
 
 export default {
 	Query: {
-		getItem: async (parent: any, args: any) => adaptResolver(makeGetItemController(), args)
+		getItem: async (parent: any, args: any) => adaptResolver(makeGetItemController(), args),
+		getItems: async (parent: any, args: any) => adaptResolver(makegetItemsController(), args)
 	}
 }
--- src/app/main/graphql/typeDefs/items.ts
+++ src/app/main/graphql/typeDefs/items.ts
@@ -3,6 +3,7 @@ import { gql } from 'apollo-server-express'
 export default gql`
 	extend type Query {
 		getItem(id: String!): Item!
+		getItems: [Item]!
 	}
 
 	type Item {
