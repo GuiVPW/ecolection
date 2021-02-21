6b32bd298a38b59b0db4f17d650fea7c7f3ad8e0
Xubili <guivpw68@gmail.com>
2021-02-21 16:52:07 -0300

    test: add prisma helper

---
+++ src/app/infra/database/prisma/helpers/prismaHelper.spec.ts
@@ -0,0 +1,19 @@
+import { PrismaHelper as sut } from './prismaHelper'
+
+describe('Prisma Helper', () => {
+	beforeAll(async () => {
+		return await sut.connect()
+	})
+
+	afterAll(async () => {
+		return await sut.disconnect()
+	})
+
+	test('Should reconnect if Prisma is down', async () => {
+		let items = await sut.findManyItems()
+		expect(items).toBeTruthy()
+		await sut.disconnect()
+		items = await sut.findManyItems()
+		await sut.disconnect()
+	})
+})
