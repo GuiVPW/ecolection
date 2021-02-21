4563434cd06bfe046d6a85a41a4e836c3edb4005
Xubili <guivpw68@gmail.com>
2021-02-21 16:50:50 -0300

    feat: add get item controller

---
+++ src/app/presentations/controllers/getItem/getItem.ts
@@ -0,0 +1,28 @@
+import { HttpRequest, HttpResponse, Controller, GetItem } from './getItemProtocols'
+import { MissingParamError, InvalidParamError } from '../../errors'
+import { badRequest, ok, serverError } from '../../helpers/http-helper'
+
+export class GetItemController implements Controller {
+	constructor(private getItem: GetItem) {}
+
+	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+		try {
+			if (!httpRequest.args.id) {
+				return badRequest(new MissingParamError('id'))
+			}
+
+			const { id } = httpRequest.args
+
+			const item = await this.getItem.get(id)
+
+			if (!item) {
+				return badRequest(new InvalidParamError('id'))
+			}
+
+			return ok({ data: item })
+		} catch (error) {
+			console.log(error)
+			return serverError()
+		}
+	}
+}
