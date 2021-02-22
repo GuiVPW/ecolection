9c8f901bad0d3b2daa8bffa1a90edbbf316a5b9f
Xubili <guivpw68@gmail.com>
2021-02-22 00:38:22 -0300

    refactor: remove routes

--- src/app/main/config/app.ts
+++ src/app/main/config/app.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
-import setupRoutes from './routes'
 import setupApollo from './apollo'
 import setupStaticFiles from './staticFiles'
 
@@ -8,7 +7,6 @@ export const createExpressApp = async () => {
 	const app = express()
 
 	setupMiddlewares(app)
-	setupRoutes(app)
 	setupStaticFiles(app)
 
 	const { server } = await setupApollo(app)
