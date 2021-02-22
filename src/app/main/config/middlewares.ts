ae94b864981e0076df39df72100681d17381dc87
Xubili <guivpw68@gmail.com>
2021-02-22 00:35:23 -0300

    feat: add helmet middleware

--- src/app/main/config/middlewares.ts
+++ src/app/main/config/middlewares.ts
@@ -1,11 +1,12 @@
 import { Express } from 'express'
 import { IN_PROD } from './constants/envs'
-import { bodyParser, contentType, cors, morgan } from '../middlewares'
+import { bodyParser, contentType, cors, morgan, helmet } from '../middlewares'
 
 export default (app: Express): void => {
 	app.use(bodyParser)
 	app.use(cors)
 	app.use(contentType)
+	app.use(helmet)
 	if (!IN_PROD) {
 		app.use(morgan)
 	}
