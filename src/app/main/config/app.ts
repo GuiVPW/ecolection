f07249d82112b6a71be2978e06b0647f6d5f8eda
Xubili <guivpw68@gmail.com>
2021-02-21 23:17:25 -0300

    feat: add app serve static files

--- src/app/main/config/app.ts
+++ src/app/main/config/app.ts
@@ -1,13 +1,15 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
-import { setupApollo } from './apollo'
+import setupApollo from './apollo'
+import setupStaticFiles from './staticFiles'
 
 export const createExpressApp = async () => {
 	const app = express()
 
 	setupMiddlewares(app)
 	setupRoutes(app)
+	setupStaticFiles(app)
 
 	const { server } = await setupApollo(app)
 
