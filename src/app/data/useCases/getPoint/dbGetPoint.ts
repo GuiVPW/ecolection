a1775e83526ec14e361bad6e7abac46f955255e8
Xubili <guivpw68@gmail.com>
2021-02-22 00:25:16 -0300

    feat: add db get point use case

---
+++ src/app/data/useCases/getPoint/dbGetPoint.ts
@@ -0,0 +1,11 @@
+import { GetPoint, PointModel, PointRepository } from './dbGetPointProtocols'
+
+export class DbGetPoint implements GetPoint {
+	constructor(private readonly getPointRepository: PointRepository) {}
+
+	async get(id: string): Promise<PointModel> {
+		const point = await this.getPointRepository.get(id)
+
+		return point
+	}
+}
---
+++ src/app/data/useCases/getPoint/dbGetPointProtocols.ts
@@ -0,0 +1,4 @@
+export * from '@domain/useCases/point/getPoint'
+export * from '@domain/models/point'
+export * from '@presentations/protocols'
+export * from '@data/protocols/pointRepository'
