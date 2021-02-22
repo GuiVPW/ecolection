c3c37d859a76c7ee4cfba34600a0090d346ec1b0
Xubili <guivpw68@gmail.com>
2021-02-22 00:28:14 -0300

    feat: add get point factory

---
+++ src/app/main/factories/point/getPoint.ts
@@ -0,0 +1,11 @@
+import { DbGetPoint } from '@data/useCases/getPoint/dbGetPoint'
+import { PointPrismaRepository } from '@infra/database/prisma/pointRepository/point'
+import { GetPointController } from '@presentations/controllers/getPoint/getPoint'
+
+export const makeGetPointController = (): GetPointController => {
+	const pointPrismaRepository = new PointPrismaRepository()
+
+	const dbGetPoint = new DbGetPoint(pointPrismaRepository)
+
+	return new GetPointController(dbGetPoint)
+}
