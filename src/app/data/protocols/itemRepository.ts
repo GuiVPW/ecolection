002bd6f878018ad781332b60029a212cd228d275
Xubili <guivpw68@gmail.com>
2021-02-24 23:23:03 -0300

    refactor: add data params to get many items repository

--- src/app/data/protocols/itemRepository.ts
+++ src/app/data/protocols/itemRepository.ts
@@ -3,5 +3,5 @@ import { ItemModel } from '@domain/models/item'
 export interface ItemRepository {
 	add?(data: Omit<ItemModel, 'id'>): Promise<ItemModel>
 	get?(data: Partial<ItemModel>): Promise<ItemModel>
-	getMany?(): Promise<ItemModel[]>
+	getMany?(data: Partial<ItemModel>): Promise<ItemModel[]>
 }
