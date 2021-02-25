9b807879b273be0650bea6ab804faeea2aee5c7f
Xubili <guivpw68@gmail.com>
2021-02-24 22:23:37 -0300

    test: add add method to item repository

---
+++ src/app/infra/database/prisma/itemRepository/item.spec.ts
@@ -0,0 +1,27 @@
+import { PrismaHelper } from '../helpers/prismaHelper'
+import { ItemPrismaRepository } from './item'
+
+describe('Item Prisma Repository', () => {
+	beforeAll(PrismaHelper.connect)
+
+	afterAll(PrismaHelper.disconnect)
+
+	beforeEach(async () => await PrismaHelper.deleteManyItems({}))
+
+	const makeSut = (): ItemPrismaRepository => {
+		return new ItemPrismaRepository()
+	}
+
+	test('Should create an item on success', async () => {
+		const sut = makeSut()
+		const item = await sut.add({
+			title: 'any_title',
+			image: 'any_image'
+		})
+
+		expect(item).toBeTruthy()
+		expect(item.id).toBeTruthy()
+		expect(item.title).toBe('any_title')
+		expect(item.image).toBe('any_image')
+	})
+})
