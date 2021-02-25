1229a1ba65996cb407cc4139f8dbcdb535a74bc6
Xubili <guivpw68@gmail.com>
2021-02-24 23:04:43 -0300

    test: add get item use case

---
+++ src/app/data/useCases/item/getItem/dbGetItem.spec.ts
@@ -0,0 +1,47 @@
+import { ItemModel, ItemRepository } from './dbGetItemProtocols'
+import { DbGetItem } from './dbGetItem'
+
+const makeItemRepository = (): ItemRepository => {
+	class ItemRepositoryStub implements ItemRepository {
+		async get(data?: Partial<ItemModel>): Promise<ItemModel> {
+			const fakeItem: ItemModel = {
+				id: 'valid_id',
+				title: 'valid_title',
+				image: 'valid_image'
+			}
+
+			return Promise.resolve(fakeItem)
+		}
+	}
+
+	return new ItemRepositoryStub()
+}
+
+interface SutTypes {
+	sut: DbGetItem
+	itemRepositoryStub: ItemRepository
+}
+
+const makeSut = (): SutTypes => {
+	const makeItemRepositoryStub = makeItemRepository()
+	const sut = new DbGetItem(makeItemRepositoryStub)
+
+	return {
+		itemRepositoryStub: makeItemRepositoryStub,
+		sut
+	}
+}
+
+describe('DbGetItem Usecase', () => {
+	test('Should return an item on success', async () => {
+		const { sut } = makeSut()
+
+		const account = await sut.get()
+
+		expect(account).toEqual({
+			id: 'valid_id',
+			title: 'valid_title',
+			image: 'valid_image'
+		})
+	})
+})
