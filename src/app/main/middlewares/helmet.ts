6641085d909f7bae57eb3a0556bd7f27ef7a57ef
Xubili <guivpw68@gmail.com>
2021-02-26 08:37:42 -0300

    fix: ensure helmet middleware permits the use of graphql playground on local server

--- src/app/main/middlewares/helmet.ts
+++ src/app/main/middlewares/helmet.ts
@@ -1,3 +1,4 @@
 import helmetMiddleware from 'helmet'
+import { IN_PROD } from '@main/config/constants/envs'
 
-export const helmet = helmetMiddleware()
+export const helmet = helmetMiddleware({ contentSecurityPolicy: !IN_PROD && false })
