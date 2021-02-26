9a0d32bbec43ec011bf4ae31e8049429014225de
Xubili <guivpw68@gmail.com>
2021-02-26 10:57:57 -0300

    refactor: unite params and payload interfaces

--- src/app/infra/database/prisma/helpers/prismaHelperProtocols.ts
+++ src/app/infra/database/prisma/helpers/prismaHelperProtocols.ts
@@ -2,63 +2,34 @@ import { Item, PointItems, Point, Prisma } from '@prisma/client'
 
 type PrismaModels = 'Item' | 'Point' | 'PointItems'
 
-type Inputs = Prisma.ItemCreateInput | Prisma.PointItemsCreateInput | Prisma.PointCreateInput
+type CreateInputs = Prisma.ItemCreateInput | Prisma.PointItemsCreateInput | Prisma.PointCreateInput
+type FindFirstArgs = Prisma.ItemFindFirstArgs | Prisma.PointItemsFindFirstArgs | Prisma.PointFindFirstArgs
+type FindManyArgs = Prisma.ItemFindManyArgs | Prisma.PointItemsFindManyArgs | Prisma.PointFindManyArgs
+type DeleteArgs = Prisma.ItemDeleteArgs | Prisma.PointItemsDeleteArgs | Prisma.PointDeleteArgs
+type DeleteManyArgs = Prisma.ItemDeleteManyArgs | Prisma.PointItemsDeleteArgs | Prisma.PointDeleteArgs
+
+type JoinedPayloads = PrismaModels extends 'Item'
+	? Item
+	: PrismaModels extends 'PointItems'
+	? PointItems
+	: Point & {
+			PointItems: (PointItems & {
+				items: Item
+			})[]
+	  }
 
 export interface Helper {
 	connect(): Promise<void>
 
 	disconnect(): Promise<void>
 
-	create(
-		model: PrismaModels,
-		data: Inputs
-	): Promise<
-		PrismaModels extends 'Item'
-			? Item
-			: PrismaModels extends 'PointItems'
-			? PointItems
-			: Point & {
-					PointItems: (PointItems & {
-						items: Item
-					})[]
-			  }
-	>
+	create(model: PrismaModels, data: CreateInputs): Promise<JoinedPayloads>
 
-	findOne(
-		model: PrismaModels,
-		params?: Prisma.ItemFindFirstArgs | Prisma.PointItemsFindFirstArgs | Prisma.PointFindFirstArgs
-	): Promise<
-		| Item
-		| PointItems
-		| (Point & {
-				PointItems: (PointItems & {
-					items: Item
-				})[]
-		  })
-		| unknown
-	>
+	findOne(model: PrismaModels, params?: FindFirstArgs): Promise<JoinedPayloads>
 
-	findMany(
-		model: PrismaModels,
-		params?: Prisma.ItemFindManyArgs | Prisma.PointItemsFindManyArgs | Prisma.PointFindManyArgs
-	): Promise<
-		| Item[]
-		| PointItems[]
-		| (Point & {
-				PointItems: (PointItems & {
-					items: Item
-				})[]
-		  })[]
-		| unknown
-	>
+	findMany(model: PrismaModels, params?: FindManyArgs): Promise<JoinedPayloads>
 
-	delete(
-		model: PrismaModels,
-		params?: Prisma.PointDeleteArgs | Prisma.ItemDeleteArgs | Prisma.PointItemsDeleteArgs
-	): Promise<boolean>
+	delete(model: PrismaModels, params?: DeleteArgs): Promise<boolean>
 
-	deleteMany(
-		model: PrismaModels,
-		params?: Prisma.PointDeleteManyArgs | Prisma.ItemDeleteManyArgs | Prisma.PointItemsDeleteManyArgs
-	): Promise<boolean>
+	deleteMany(model: PrismaModels, params?: DeleteManyArgs): Promise<boolean>
 }
