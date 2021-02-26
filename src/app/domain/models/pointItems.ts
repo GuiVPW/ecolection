6f7f2c8171b6103771f32f5922b1db65ca623ad0
Xubili <guivpw68@gmail.com>
2021-02-26 17:37:12 -0300

    feat: add items and points interface

--- src/app/domain/models/pointItems.ts
+++ src/app/domain/models/pointItems.ts
@@ -1,5 +1,10 @@
+import { ItemModel } from './item'
+import { PointModel } from './point'
+
 export interface PointItemsModel {
-	id: string
+	id?: string
 	pointsId: string
 	itemsId: string
+	items?: ItemModel
+	points?: Omit<PointModel, 'items'>
 }
