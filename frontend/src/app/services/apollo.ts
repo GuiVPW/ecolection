0bfd9b581465f72b236f0feff903917f050c20cd
Xubili <guivpw68@gmail.com>
2021-02-26 19:21:04 -0300

    refactor: remove unused links

--- frontend/src/app/services/apollo.ts
+++ frontend/src/app/services/apollo.ts
@@ -1,24 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
-import { from, split } from '@apollo/client/link/core'
-import { getMainDefinition } from '@apollo/client/utilities'
-import { createUploadLink } from 'apollo-upload-client'
 
-import baseUrl from '../app/constants/apiUrl'
-
-const uploadLink = createUploadLink({
-	uri: `${baseUrl}/graphql`
-})
-
-export const splitLink = split(({ query }) => {
-	const definition = getMainDefinition(query)
-	return (
-		definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
-	)
-}, uploadLink)
-
-const link = from([splitLink])
+import baseUrl from '../constants/apiUrl'
 
 export const client = new ApolloClient({
-	link,
+	uri: `${baseUrl}/graphql`,
 	cache: new InMemoryCache()
 })
