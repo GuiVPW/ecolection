fc91e2e1da0aa3dab520e4fc7d7606de19b35e08
Xubili <guivpw68@gmail.com>
2021-02-26 18:11:26 -0300

    feat: add create point controller factory

--- src/app/main/factories/point/createPoint.ts
+++ src/app/main/factories/point/createPoint.ts
@@ -1,11 +1,19 @@
-import { DbCreatePoint } from '@data/useCases/createPoint/dbCreatePoint'
+import { DbCreatePoint } from '@data/useCases/point/createPoint/dbCreatePoint'
+import { ItemPrismaRepository } from '@infra/database/prisma/itemRepository/item'
+import { PointItemsPrismaRepository } from '@infra/database/prisma/pointItemsRepository/pointItems'
 import { PointPrismaRepository } from '@infra/database/prisma/pointRepository/point'
-import { CreatePointController } from '@presentations/controllers/createPoint/createPoint'
+import { CreatePointController } from '@presentations/controllers/point/createPoint/createPoint'
 
 export const makeCreatePointController = (): CreatePointController => {
-	const itemPrismaRepository = new PointPrismaRepository()
+	const pointPrismaRepository = new PointPrismaRepository()
+	const itemPrismaRepository = new ItemPrismaRepository()
+	const pointItemsPrismaRepository = new PointItemsPrismaRepository()
 
-	const dbCreatePoint = new DbCreatePoint(itemPrismaRepository)
+	const dbCreatePoint = new DbCreatePoint(
+		pointPrismaRepository,
+		itemPrismaRepository,
+		pointItemsPrismaRepository
+	)
 
 	return new CreatePointController(dbCreatePoint)
 }
