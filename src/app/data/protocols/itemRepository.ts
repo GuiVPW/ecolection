51448fd8b20ef33aba2dc7790c22e50e5bbf5c7a
Xubili <guivpw68@gmail.com>
2021-02-26 17:34:56 -0300

    refactor: change add to create interface

--- src/app/data/protocols/itemRepository.ts
+++ src/app/data/protocols/itemRepository.ts
@@ -1,7 +1,7 @@
 import { ItemModel } from '@domain/models/item'
 
 export interface ItemRepository {
-	add?(data: Omit<ItemModel, 'id'>): Promise<ItemModel>
+	create?(data: Omit<ItemModel, 'id'>): Promise<ItemModel>
 	get?(data: Partial<ItemModel>): Promise<ItemModel>
 	getMany?(data: Partial<ItemModel>): Promise<ItemModel[]>
 	deleteMany?(): Promise<boolean>
