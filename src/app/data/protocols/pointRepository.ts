f65ab059b5a203b6832f2eb31f3a6db71e07157c
Xubili <guivpw68@gmail.com>
2021-02-26 10:48:08 -0300

    feat: add point repository get and delete many methods

--- src/app/data/protocols/pointRepository.ts
+++ src/app/data/protocols/pointRepository.ts
@@ -1,6 +1,7 @@
 import { PointModel } from '@domain/models/point'
 
 export interface PointRepository {
-	create(data: Omit<PointModel, 'id' | 'items'> & { items: string[] }): Promise<Omit<PointModel, 'items'>>
-	get(id: string): Promise<PointModel>
+	create(data: Omit<PointModel, 'id' | 'items'>): Promise<Omit<PointModel, 'items'>>
+	get(data?: Partial<PointModel>): Promise<PointModel>
+	deleteMany(): Promise<boolean>
 }
