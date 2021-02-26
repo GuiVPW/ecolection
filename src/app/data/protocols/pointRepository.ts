d6fa75ccf4a95cfa0db85879a8e353de9ed3cea8
Xubili <guivpw68@gmail.com>
2021-02-26 15:59:56 -0300

    refactor: update point repository types

--- src/app/data/protocols/pointRepository.ts
+++ src/app/data/protocols/pointRepository.ts
@@ -1,7 +1,7 @@
 import { PointModel } from '@domain/models/point'
 
 export interface PointRepository {
-	create(data: Omit<PointModel, 'id' | 'items'>): Promise<Omit<PointModel, 'items'>>
-	get(data?: Partial<PointModel>): Promise<PointModel>
-	deleteMany(): Promise<boolean>
+	create?(data: Omit<PointModel, 'id' | 'items'>): Promise<Omit<PointModel, 'items'>>
+	get?(data?: Partial<PointModel>): Promise<PointModel>
+	deleteMany?(): Promise<boolean>
 }
