11c0427a722467db424376aac2315881b037b985
Xubili <guivpw68@gmail.com>
2021-02-26 15:58:39 -0300

    feat: add not found error

--- src/app/data/useCases/point/getPoint/dbGetPoint.ts
+++ src/app/data/useCases/point/getPoint/dbGetPoint.ts
@@ -1,10 +1,15 @@
+import { notFoundError } from '@presentations/helpers/http-helper'
 import { GetPoint, PointModel, PointRepository } from './dbGetPointProtocols'
 
 export class DbGetPoint implements GetPoint {
 	constructor(private readonly getPointRepository: PointRepository) {}
 
-	async get(id: string): Promise<PointModel> {
-		const point = await this.getPointRepository.get({ id })
+	async get(params?: Partial<Omit<PointModel, 'items'>>): Promise<PointModel> {
+		const point = await this.getPointRepository.get(params)
+
+		if (!point) {
+			throw notFoundError(new Error('Could not find post with given parameters.'))
+		}
 
 		return point
 	}
