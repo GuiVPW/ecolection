c1cb942654379a503463f1c92f1ab5c0b710810b
Xubili <guivpw68@gmail.com>
2021-02-27 02:24:05 -0300

    feat: add toastify configs

---
+++ frontend/src/app/utils/toastify/config.ts
@@ -0,0 +1,10 @@
+import { ToastOptions } from 'react-toastify'
+
+export const toastOptions: ToastOptions = {
+	autoClose: 5000,
+	hideProgressBar: false,
+	closeOnClick: true,
+	pauseOnHover: true,
+	draggable: true,
+	progress: undefined
+}
